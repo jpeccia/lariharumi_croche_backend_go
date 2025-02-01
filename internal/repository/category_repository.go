60fb793a63fa49038a53ff3385b9449d0b5509bd
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-01 19:02:38 -0300

    feat; create category repository

---
+++ internal/repository/category_repository.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"github.com/jpeccia/lariharumi_croche_backend_go/config"
+	"github.com/jpeccia/lariharumi_croche_backend_go/internal/model"
+	"gorm.io/gorm"
+)
+
+// CreateCategory cria uma nova categoria no banco de dados
+func CreateCategory(category *model.Category) error {
+	if err := config.DB.Create(category).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetCategories retorna todas as categorias
+func GetCategories() ([]model.Category, error) {
+	var categories []model.Category
+	if err := config.DB.Find(&categories).Error; err != nil {
+		return nil, err
+	}
+	return categories, nil
+}
+
+// GetCategoryByID retorna uma categoria pelo seu ID
+func GetCategoryByID(categoryID uint) (*model.Category, error) {
+	var category model.Category
+	if err := config.DB.First(&category, categoryID).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &category, nil
+}
+
+// DeleteCategory deleta uma categoria pelo seu ID
+func DeleteCategory(categoryID uint) error {
+	if err := config.DB.Delete(&model.Category{}, categoryID).Error; err != nil {
+		return err
+	}
+	return nil
+}
