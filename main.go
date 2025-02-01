3e8f5795ad55c6548d52bc3a01761e1ed65bf3eb
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-01 17:50:37 -0300

    feat: pre-configurate main file

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package main;
+package main
+
+import (
+	"log"
+
+	"github.com/jpeccia/lariharumi_croche_backend_go/config"
+)
+
+func main() {
+	config.ConnectDB()
+
+	log.Println("Servidor rodando na porta 8080....")
+}
