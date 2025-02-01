b39c1c10ead1e4a6b2090e93b573b0b30b8b77de
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-01 17:47:48 -0300

    feat: configurate load env and db connect

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Erro ao carregar .env")
+	}
+}
+
+func ConnectDB() {
+	LoadEnv()
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Falha ao conectar ao banco de dados", err)
+	}
+
+	fmt.Println("Banco de dados conectado!")
+}
