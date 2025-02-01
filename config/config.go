a2271197f06de7b14e68e8fd0b45f7dc8d334e23
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-01 18:31:49 -0300

    feat: create docker-compose

--- config/config.go
+++ config/config.go
@@ -23,7 +23,7 @@ func ConnectDB() {
 	LoadEnv()
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
 
 	var err error
