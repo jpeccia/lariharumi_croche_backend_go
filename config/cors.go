e5c55cd74fb207330c34fbe5b360f1861157081f
João Otávio Peccia <joaootaviopeccia0@@gmail.com>
2025-02-11 18:30:18 -0300

    feat: config cors

--- config/cors.go
+++ config/cors.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"log"
 	"os"
 
 	"github.com/gin-contrib/cors"
@@ -10,11 +11,13 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	frontendURL := os.Getenv("FRONTEND_URL")
 
+	log.Println("CORS AllowOrigins:", frontendURL)
+
 	config := cors.Config{
-		AllowOrigins:     []string{frontendURL}, 
+		AllowOrigins:     []string{frontendURL},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization", "X-Requested-With"},
-		AllowCredentials: true, 
+		AllowCredentials: true,
 	}
 
 	return cors.New(config)
--- internal/router/router.go
+++ internal/router/router.go
@@ -10,42 +10,41 @@ import (
 func SetupRouter() *gin.Engine {
 	r := gin.Default()
 
-	r.Static("/uploads", "./uploads")
-
-	// Aplica o middleware de CORS globalmente
 	r.Use(config.CORSMiddleware())
 
-	// Grupo de autenticação
+	r.OPTIONS("/*any", func(c *gin.Context) {
+		c.Status(204)
+	})
+
+	r.Static("/uploads", "./uploads")
+
 	auth := r.Group("/auth")
 	{
 		auth.POST("/register", handler.RegisterUser)
 		auth.POST("/login", handler.LoginUser)
 	}
 
-	// Grupo público, sem autenticação
 	categories := r.Group("/categories")
 	{
-		categories.GET("", handler.GetCategories) // Rota sem a barra final
+		categories.GET("", handler.GetCategories)
 		categories.GET("/:id/image", handler.GetCategoryImage)
 	}
 
 	products := r.Group("/products")
 	{
-		products.GET("/category/:id", handler.GetProductsByCategory) // Rota para obter produtos por categoria
-		products.GET("", handler.GetProducts)                        // GET products sem barra final
-		products.GET("/:id/images", handler.GetProductImages)        // GET product images
+		products.GET("/category/:id", handler.GetProductsByCategory)
+		products.GET("", handler.GetProducts)
+		products.GET("/:id/images", handler.GetProductImages)
 	}
 
-	// Rota protegida para admin (somente para admin)
 	admin := r.Group("").Use(middleware.AuthMiddleware("ADMIN"))
 	{
-		admin.POST("/products", handler.CreateProduct)          // POST create product
-		admin.PATCH("/products/:id", handler.UpdateProduct)     // UPDATE product
-		admin.PUT("/categories/:id", handler.UpdateCategory)    // UPDATE category
-		admin.DELETE("/products/:id", handler.DeleteProduct)    // DELETE product
-		admin.POST("/categories", handler.CreateCategory)       // POST create category
-		admin.DELETE("/categories/:id", handler.DeleteCategory) // DELETE category
-		// Rotas para upload de imagem
+		admin.POST("/products", handler.CreateProduct)
+		admin.PATCH("/products/:id", handler.UpdateProduct)
+		admin.PUT("/categories/:id", handler.UpdateCategory)
+		admin.DELETE("/products/:id", handler.DeleteProduct)
+		admin.POST("/categories", handler.CreateCategory)
+		admin.DELETE("/categories/:id", handler.DeleteCategory)
 		admin.POST("/products/:id/upload-image", handler.UploadProductImage)
 		admin.POST("/categories/:id/upload-image", handler.UploadCategoryImage)
 		admin.DELETE("/products/:id/images/:index", handler.DeleteProductImage)
